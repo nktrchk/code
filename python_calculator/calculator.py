c60a28d63dad99b0b9952da54101f7bff5833495
nktrchk <nktrchk@users.noreply.github.com>
2023-02-24 09:47:35 +0100

    commit 1

--- python_calculator/calculator.py
+++ python_calculator/calculator.py
@@ -3,14 +3,11 @@
 def get_trip_price(distance,gas_prise, consunption):
     calculation_result = distance*(consunption/100*gas_prise)
 
-    if calculation_result <= 100:
+    if calculation_result <= 99:
         print(f'It s going to be cheap')
         print(calculation_result, "EUR We will spend to get there")
-    if calculation_result > 200:
+    elif calculation_result > 101:
         print("It's okaysch"),
-        print(calculation_result, "EUR We will spend to get there")
-    else:
-        print('Are you fuckin kidding me?')
         print(calculation_result, "EUR We will spend to get there")
 
 
